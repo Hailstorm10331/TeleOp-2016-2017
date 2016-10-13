1ef43cbcb7e18d24b6027a9b12927502a9733d77
Alex Zeng <azeng68@gmail.com>
2016-10-13 00:15:27 -0400

    Add files via upload
    So far, the file covers changes to the way opmodes are registered and shown that are new to version 2 of the ftc_app.

---
+++ ThunderBasicAuto2016_2017.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Alex on 10/12/2016.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//package org.firstinspires.ftc.robotcontroller.external.samples;
+
+
+@TeleOp(name="Basic Autonomous", group="Autonomous")
+/**The line above demonstrates the new way to register opmodes inside the code. This method cannot
+ * be used in FTC apps prior to version 2. (Update your phones to have matching version numbers
+ * and version 2 of the FTC apps.)
+ */
+//@Disabled
+/**The "@Disabled" in the line above this comment indicates whether or not the opmode will be
+ * shown in the opmode list inside the Driver Station app.
+ * */
+
+public class ThunderBasicAuto2016_2017 {
+    // Code goes here! Register autonomous functions outside of this.
+}
